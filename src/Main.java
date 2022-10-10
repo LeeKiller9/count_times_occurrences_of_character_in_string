9e1583104e5edffe25ef58ccc76ade7542560938
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-10-10 14:07:46 +0700

    update feature

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static int count = 0;
+    static ArrayList<Integer> arrayIndex = new ArrayList<>();
     public static void main(String[] args) {
-        String txt = "Immigrant aworkeras face wage theft and unsafe conditions as they rebuild Florida";
+        String txt = "Immigrant workers face wage theft and unsafe conditions as they rebuild Florida";
         System.out.println("The string is: " + txt);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter character want to count: ");
-        char c = scanner.next().charAt(0); // ??? khai báo 1 char
-        ArrayList<Integer> arrayIndex = new ArrayList<>(); // sử dụng arraylist để có thể thêm gtrị khi chưa bik size array
-        int count = 0;
+//      char c = (char) scanner.nextInt();
+//      System.out.print("C:" + c);
+        char c = scanner.next().charAt(0);
+        searchCharacter(c,txt);
+        System.out.println("The character '" + c + "' occurrence " + count + " time");
+        System.out.print("The position: ");
+        for (int a : arrayIndex) {
+            System.out.print("\t" + a);
+        }
+    }
+    public static void searchCharacter(char c, String txt) {
         for (int i = 0; i < txt.length(); i++) {
             if (c == txt.charAt(i)) {
                 arrayIndex.add(i);
                 count++;
             }
-        }
-        System.out.println("The character '" + c + "' occurrence " + count + " time");
-        System.out.print("The position: ");
-        for (int a : arrayIndex) {
-            System.out.print("\t" + a);
         }
     }
 }
